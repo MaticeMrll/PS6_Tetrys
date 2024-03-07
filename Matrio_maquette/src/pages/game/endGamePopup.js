c71a6738fd1a0e722a2449ee4f9237f93368cc34
Rom1420 <romain.abbonato@etu.univ-cotedazur.fr>
2024-03-07 18:55:05 +0100

    Feat : added the end game window

--- Matrio_maquette/src/pages/game/endGamePopup.js
+++
@@ -1,7 +0,0 @@
-
-document.getElementById('end-game-button').addEventListener('click', function() {
-    var iframe = document.createElement('iframe');
-    iframe.src = './endGameStat.html';
-    iframe.className = 'end-stat';
-    document.body.appendChild(iframe);
-});
